8e81418da2a23785a0f484eebdc1b28dab837059
Alex Cocan <al_razvan@yahoo.com>
2020-07-15 00:11:08 +0200

    Updates

---
+++ precache-manifest.7b077193b185c252826bdd981f5a51e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cbc835e7cfa5ec71e191715173e3119",
+    "url": "/React-Robofriends-App-Portfolio-App/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/React-Robofriends-App-Portfolio-App/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bd9840955a4016cc62b4",
+    "url": "/React-Robofriends-App-Portfolio-App/static/css/main.1637b7e8.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/React-Robofriends-App-Portfolio-App/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Robofriends-App-Portfolio-App/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd9840955a4016cc62b4",
+    "url": "/React-Robofriends-App-Portfolio-App/static/js/main.c084603a.chunk.js"
+  },
+  {
+    "revision": "9f57c04cb0f7be468738",
+    "url": "/React-Robofriends-App-Portfolio-App/static/js/runtime-main.3ef50beb.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/React-Robofriends-App-Portfolio-App/static/media/SEGA.12a3db8b.woff"
+  }
+]);
